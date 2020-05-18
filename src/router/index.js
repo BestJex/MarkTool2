a37e12c034c56234757780262f325825089786ed
Xiaozhou9035 <903597485@qq.com>
2020-05-18 18:22:58 +0800

    add:路径修改

--- src/router/index.js
+++ src/router/index.js
@@ -550,6 +550,7 @@ export const asyncRoutes = [
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
+  base: '/new/',
   routes: constantRoutes
 })
 
